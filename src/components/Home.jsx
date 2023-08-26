e9821f8ff9e821c2f7f814e0eca5877f48ec4370
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-26 21:57:08 +0100

    Add some short text to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,11 @@ const Home = () => {
   return (
     <>
       <h2>Home</h2>
+      <p>Welcome to zhOOpers! You can&apos;t actually buy anything here...</p>
+      <p>
+        Which is awesome news because you get to keep your money instead of
+        spending it all on nonsense. Hurray!
+      </p>
     </>
   );
 };

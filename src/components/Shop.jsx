6cb21a287661e6c012e27553d59e0f9148858e45
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-26 22:16:59 +0100

    Create item showcase and show it on the shop

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+import ShowCase from './ShowCase';
+
 const Shop = () => {
+  const [numberOfItems, setNumberOfItems] = useState(0);
+
   return (
     <>
       <h2>Shop</h2>
+      <div className="cart-info">
+        <p>Item in the cart: {numberOfItems}</p>
+        <button type="button" className="open-cart">
+          View Items
+        </button>
+        <ShowCase />
+      </div>
     </>
   );
 };
---
+++ src/components/ShowCase.jsx
@@ -0,0 +1,33 @@
+const ShowCase = () => {
+  // TODO: Replace items with the ones fetched from the API
+  const items = [
+    {
+      id: 1,
+      name: 'Apple',
+      price: 1.14,
+    },
+    {
+      id: 2,
+      name: 'Banana',
+      price: 1.42,
+    },
+    {
+      id: 3,
+      name: 'Mango',
+      price: 3.06,
+    },
+  ];
+
+  return (
+    <div className="showcase">
+      {items.map((item) => (
+        <div key={item.id}>
+          <p>{item.name}</p>
+          <p>{item.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ShowCase;

ea1bbd0b5af0429117f8af1be8d6523c0ab30f5a
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-27 01:43:10 +0100

    Add some text to home and style it a bit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,25 @@
+import '../styles/Home.css';
+
 const Home = () => {
   return (
-    <>
+    <div className="welcome">
       <h2>Home</h2>
-      <p>Welcome to zhOOpers! You can&apos;t actually buy anything here...</p>
-      <p>
-        Which is awesome news because you get to keep your money instead of
-        spending it all on nonsense. Hurray!
-      </p>
-    </>
+      <div className="introduction">
+        <p>Welcome to zhOOpers! You can&apos;t actually buy anything here...</p>
+        <p>
+          Which is awesome news because you get to keep your money instead of
+          spending it all on nonsense. Hurray!
+        </p>
+        <p>
+          It&apos;s also important to note that the dev was making this at 1 AM
+          and after finishing the shop page he decided to leave this blank.
+        </p>
+        <p>
+          If you want to see some interesting stuff (maybe???) go click on
+          &quot;Shop&quot; on the navigation bar. Good night.
+        </p>
+      </div>
+    </div>
   );
 };
 
